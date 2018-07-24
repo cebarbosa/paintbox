42144373cda503b8c46ea06c460fda808cf3dd37
cebarbosa <carlos.barbosa@usp.br>
2018-07-24 17:09:43 -0300

    Refactoring of project.

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+""" 
+
+Created on 06/07/18
+
+Author : Carlos Eduardo Barbosa
+
+"""
+from setuptools import setup
+
+setup(name='bsf',
+      version='0.1',
+      description='Bayesian Spectral Fitting',
+      url='http://github.com/cebarbosa/bsf',
+      author='Carlos Eduardo Barbosa',
+      author_email='kadu.barbosa@gmail.com',
+      license='MIT',
+      packages=['bsf'],
+      zip_safe=False)
