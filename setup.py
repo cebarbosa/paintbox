9d8c18b378f058efcf0631060e61253b91913921
cebarbosa <carlos.barbosa@usp.br>
2020-07-14 16:28:04 -0300

    Remove compliled extensions from setup.py.

--- setup.py
+++ setup.py
@@ -9,9 +9,6 @@ import sys
 
 from setuptools import setup
 
-from extension_helpers import get_extensions
-
-
 # First provide helpful messages if contributors try and run legacy commands
 # for tests or docs.
 
@@ -77,5 +74,4 @@ except Exception:
 """.lstrip()
 
 setup(use_scm_version={'write_to': os.path.join('paintbox', 'version.py'),
-                       'write_to_template': VERSION_TEMPLATE},
-      ext_modules=get_extensions())
+                       'write_to_template': VERSION_TEMPLATE})
