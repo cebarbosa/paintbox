596abf3b5aa32ed56b2c965591ea535007dc6670
cebarbosa <carlos.barbosa@usp.br>
2020-09-28 18:06:28 -0300

    Trying to avoid error in pip.

--- setup.py
+++ setup.py
@@ -69,7 +69,11 @@ VERSION_TEMPLATE = """
 # Note that we need to fall back to the hard-coded version if either
 # setuptools_scm can't be imported or setuptools_scm can't determine the
 # version, so we catch the generic 'Exception'.
-version = '{version}'
+try:
+    from setuptools_scm import get_version
+    version = get_version(root='..', relative_to=__file__).split("+")[0]
+except Exception:
+    version = '{version}'.split("+")[0]
 """.lstrip()
 
 setup(use_scm_version={'write_to': os.path.join('paintbox', 'version.py'),
