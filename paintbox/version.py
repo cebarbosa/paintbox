4d3576b744608694ef124dbec9e37f1cf82b8801
cebarbosa <kadu.barbosa@gmail.com>
2021-04-08 17:31:33 -0300

    Included correct version number.

--- paintbox/version.py
+++ paintbox/version.py
@@ -1,8 +1,1 @@
-# Note that we need to fall back to the hard-coded version if either
-# setuptools_scm can't be imported or setuptools_scm can't determine the
-# version, so we catch the generic 'Exception'.
-try:
-    from setuptools_scm import get_version
-    version = get_version(root='..', relative_to=__file__).split("+")[0]
-except Exception:
-    version = '1.0.1.dev8'.split("+")[0]
+version = "1.1.1"
