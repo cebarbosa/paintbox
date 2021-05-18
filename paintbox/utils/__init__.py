aeb22629b74e37231832f4206aaed6270c758393
cebarbosa <kadu.barbosa@gmail.com>
2021-05-18 16:21:54 -0300

    Trying file similar to main package one.

--- paintbox/utils/__init__.py
+++ paintbox/utils/__init__.py
@@ -2,13 +2,9 @@
 
 # This sub-module is destined for common non-package specific utility
 # functions.
+__all__ = []
+
 from .disp2vel import *
 from .convolve import *
 from .CvD_utils import *
 from .lick import *
-
-__all__ = []
-__all__ += disp2vel.__all__
-__all__ += convolve.__all__
-__all__ += CvD_utils.__all__
-__all__ += lick.__all__
