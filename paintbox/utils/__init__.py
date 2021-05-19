0eed76fa7fb2c22d0c84190dec1e574be40c2b67
cebarbosa <kadu.barbosa@gmail.com>
2021-05-19 09:13:35 -0300

    Working in imports.

--- paintbox/utils/__init__.py
+++ paintbox/utils/__init__.py
@@ -1,15 +1,20 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import sys
+from pathlib import Path
+file = Path(__file__). resolve()
+package_root_directory = file.parents [1]
+sys.path.append(str(package_root_directory))
 
 # This sub-module is destined for common non-package specific utility
 # functions.
 __all__ = []
 
-from .disp2vel import *
-from .convolve import *
-from .CvD_utils import *
-from .lick import *
+import disp2vel
+import convolve
+import lick
+import CvD_utils
 
-__all__ += ["disp2vel"]
+__all__ += disp2vel.__all__
 __all__ += convolve.__all__
-__all__ += CvD_utils.__all__
 __all__ += lick.__all__
+__all__ += CvD_utils.__all__
