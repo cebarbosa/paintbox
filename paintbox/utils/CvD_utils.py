9373d3966c1e9a7b4fa67cf06c598cc95aad909d
cebarbosa <kadu.barbosa@gmail.com>
2021-05-20 13:40:45 -0300

    Updated docs for CvD class, simplified use.

--- paintbox/utils/CvD_utils.py
+++ paintbox/utils/CvD_utils.py
@@ -1,5 +1,6 @@
 """ Calculates the M/L for CvD models. """
 import os
+import glob
 
 import numpy as np
 import astropy.units as u
@@ -18,86 +19,125 @@ __all__ = ["CvD18"]
 class CvD18():
     """ Utility class for the use of CvD models.
 
-    This class provides a convenient interface to CvD models, including both
-    the base SSP models and also response functions for several chemical
-    elements.
+    This class provides a convenience `paintbox` interface to CvD models,
+    producing a callable object that can be used together with other classes
+    to produce more detailed SED models.
+
+    The CvD model files require pre-processing for use with `paintbox`,
+    which may require an initial overhead of ~one minute, but the processed
+    files can be reused for several files / runs if necessary it models are
+    stored in disk.
+
 
     Parameters
     ----------
-    wave:
+    wave: ndarray, astropy.units.Quantity
+        Wavelenght array of the model.
 
-    ssp_files:
-
-    rf_files:
+    libpath: str
+        Path to the locations where CvD18 models are stored. By default, it
+        looks for files inside paintbox/paintbox/extern/CvD18. Both SSP and
+        response function files are assumed to be uncompressed inside this
+        directory.
 
     sigma: float
+        Velocity dispersion of the output in km/s. Default is100 km/s.
+
+    store: bool
+        Option to store processed models in disk. Default is True.
+
+    outdir: str
+        Location where processed models are stored.
+
+    use_stored: bool
+        Option to use stored models. Default is True.
+
+    elements: list
+        Chemical abundances to be included in the model. Available elements are
+        'Ba', 'C', 'Ca', 'Co', 'Cr', 'Cu', 'Eu', 'Fe', 'K', 'Mg', 'Mn', 'N',
+        'Na', 'Ni', 'Si', 'Sr', 'T', 'Ti', 'V', 'a/Fe', 'as/Fe'. Default is
+        to use all elements except T (temperature of hot star) and 'a/Fe'.
+
+    norm: bool
+        Normalize the flux of the processed models to the median. Default is
+        True.
+
     Attributes
     ----------
     parnames: list
-        Name of the templates.
+        Name of the parameters in the model.
+
 
 
 
     """
-    def __init__(self, wave, ssp_files=None, rf_files=None, sigma=100,
-                 store=True, outdir=None, outname=None, use_stored=True,
-                 elements=None, wprefix=None, norm=True):
+    def __init__(self, wave, libpath=None, sigma=100, store=True, outdir=None,
+                 use_stored=True, elements=None, norm=True):
         if hasattr(wave, "unit"):
-            wave = wave.to(u.Angstrom).value
-        self.wave = wave
-        self.ssp_files = ssp_files
-        self.rf_files = [] if rf_files is None else rf_files
-        self.wprefix = "w" if wprefix is None else wprefix
+            self.wave = wave.to(u.Angstrom).value
+        else:
+            self.wave = wave #Assumes units are Angstrom
+        assert wave.min() >= 3501, "Minimum wavelength is 3501 Angstrom"
+        assert wave.max() <= 25000, "Maximum wavelength is 25000 Angstrom"
         assert sigma >= 100, "Minumum velocity dispersion for models is 100 " \
                              "km/s."
         self.sigma = sigma
         self.store = store
+        # Defining output names
+        wmin, wmax = int(self.wave.min()), int(self.wave.max())
         self.outdir = os.getcwd() if outdir is None else outdir
         if not os.path.exists(self.outdir):
             os.mkdir(self.outdir)
-        self.outname = "CvD18" if outname is None else outname
-        # Processing and reading stellar population models
-        self.ssp_file = os.path.join(self.outdir,
-                                     "{}_sps.fits".format(self.outname))
+        self.outprefix = os.path.join(self.outdir,
+                         f"CvD18_sig{self.sigma}_w{wmin}-{wmax}")
+        #
+        ext_path = os.path.split(os.path.dirname(os.path.realpath(__file__)))[0]
+        default_path = os.path.join(ext_path, "extern/CvD18")
+        self.libpath = default_path if libpath is None else libpath
+        # Processing SSP files if necessary
+        self.ssp_file = f"{self.outprefix}_SSPs.fits"
         if not os.path.exists(self.ssp_file) or not use_stored:
-            assert self.ssp_files is not None, "SSP files have to be provided."
-            self.templates, self.params = self.prepare_CvD18_ssps(ssp_files,
-                                          wave, self.ssp_file, sigma=self.sigma)
+            self.ssp_files = glob.glob(os.path.join(self.libpath, "VCJ*.s100"))
+            if len(self.ssp_files) == 0:
+                raise ValueError("Stellar populations not found in libpath.")
+            self.templates, self.params = self._prepare_CvD18_ssps()
             if store:
                 self.write(self.wave, self.params, self.templates,
                            self.ssp_file)
         else:
             self.params, self.templates = self._read(self.ssp_file)
+        # Normalizing models if required
         self.norm = 1.
         if norm:
             self.norm = np.median(self.templates, axis=1)
             self.templates /= self.norm[:, np.newaxis]
-        self.limits = {}
+        # Setting limits of the models
+        self._limits = {}
         for param in self.params.colnames:
             vmin = self.params[param].data.min()
             vmax = self.params[param].data.max()
-            self.limits[param] = (vmin, vmax)
-        # Processing and reading response functions
+            self._limits[param] = (vmin, vmax)
+        # Processing response functions
+        self.rf_infiles = glob.glob(os.path.join(self.libpath,
+                                                 "atlas_ssp*.s100"))
         self._all_elements = ['Ba', 'C', 'Ca', 'Co', 'Cr', 'Cu', 'Eu', 'Fe',
                               'K', 'Mg', 'Mn', 'N', 'Na', 'Ni', 'Si', 'Sr',
                               'T', 'Ti', 'V', 'a/Fe', 'as/Fe']
-        self.elements = ['Ba', 'C', 'Ca', 'Co', 'Cr', 'Cu', 'Eu', 'Fe',
-                         'K', 'Mg', 'Mn', 'N', 'Na', 'Ni', 'Si', 'Sr',
-                         'Ti', 'V'] if elements is None else elements
-        rfprefix = os.path.join(self.outdir, self.outname)
-        rfout = ["{}_{}.fits".format(rfprefix, el.replace("/", ":")) for el in
-                 self.elements]
-        if not all([os.path.exists(f) for f in rfout]):
-            self.rfs, self.rfpars = self.prepare_CvD18_respfun(rf_files,
-                                                             self.wave,
-                                    outprefix=rfprefix, sigma=self.sigma)
-            if store:
-                for element, fname in zip(self.elements, rfout):
+        self.elements = ["C", "N", "Na", "Mg", "Si", "Ca", "Ti", "Fe", "K",
+                         "Cr", "Mn", "Ba", "Ni", "Co", "Eu", "Sr", "V", "Cu",
+                         "as/Fe"] if elements is None else elements
+        self.rf_outfiles = [    "{}_{}.fits".format(self.outprefix,
+                            el.replace("/", ":")) for
+                            el in self._all_elements]
+        if not all([os.path.exists(f) for f in self.rf_outfiles]):
+            self.rfs, self.rfpars = self._prepare_CvD18_respfun()
+            if self.store:
+                for element, fname in zip(self.elements, self.rf_outfiles):
                     self.write(self.wave, self.rfpars[element], self.rfs[
                                element], fname)
         else:
             self.rfs, self.rfpars = {}, {}
-            for element, fname in zip(self.elements, rfout):
+            for element, fname in zip(self.elements, self.rf_outfiles):
                 self.rfpars[element], self.rfs[element] = self._read(fname)
         # Build model with paintbox
         ssp = ParametricModel(self.wave, self.params, self.templates)
@@ -107,15 +147,15 @@ class CvD18():
             ssp = ssp * rf
             vmin = rf.params[element].data.min()
             vmax = rf.params[element].data.max()
-            self.limits[element] = (vmin, vmax)
+            self._limits[element] = (vmin, vmax)
         if len(self.elements) > 0: # Update limits in case response functions
             # are used.
             for p in ["Age", "Z"]:
                 vmin = rf.params[p].data.min()
                 vmax = rf.params[p].data.max()
-                self.limits[p] = (vmin, vmax)
+                self._limits[p] = (vmin, vmax)
         self._interpolator = Constrain(ssp)
-        self.parnames = self._interpolator.parnames
+        self._parnames = self._interpolator.parnames
         self._nparams = len(self.parnames)
 
     def __call__(self, theta):
@@ -129,8 +169,8 @@ class CvD18():
         """  Multiplication between two SED components. """
         return CompositeSED(self, o, "*")
 
-    def prepare_CvD18_ssps(self, ssp_files, wave, output, sigma=100):
-        """ Prepare templates for SSP models from Villaume et al. (2017).
+    def _prepare_CvD18_ssps(self):
+        """ Process SSP models.
 
         Parameters
         ----------
@@ -141,26 +181,17 @@ class CvD18():
             wavelength is provided as as numpy array.
         output: str
             Name of the output file (a multi-extension FITS file)
-        sigma: float
-            Velocity dispersion of the models, in km/s. Defaults to 100 km/s,
-            the minimum resolution of the models.
         store: bool
             Option to store
 
         """
-        if hasattr(wave, "unit"):
-            wave = wave.to(u.Angstrom).value
-        assert wave[0] >= 3501, "Minimum wavelength is 3501 Angstrom"
-        assert wave[-1] <= 25000, "Maximum wavelength is 25000 Angstrom"
-        assert all([os.path.exists(f) for f in ssp_files]), \
-               "Some or all input files are missing."
         nimf = 16
         imfs = 0.5 + np.arange(nimf) / 5
         x2s, x1s=  np.stack(np.meshgrid(imfs, imfs)).reshape(2, -1)
-        velscale = int(sigma / 4)
-        kernel_sigma = np.sqrt(sigma ** 2 - 100 ** 2) / velscale
+        velscale = int(self.sigma / 4)
+        kernel_sigma = np.sqrt(self.sigma ** 2 - 100 ** 2) / velscale
         ssps, params = [], []
-        for fname in tqdm(ssp_files, desc="Processing SSP files"):
+        for fname in tqdm(self.ssp_files, desc="Processing SSP files"):
             spec = os.path.split(fname)[1]
             T = float(spec.split("_")[3][1:])
             Z = float(spec.split("_")[4][1:-8].replace("p", "+").replace(
@@ -170,16 +201,16 @@ class CvD18():
                                     names=["Z", "Age", "x1", "x2"]))
             data = np.loadtxt(fname)
             w = data[:,0]
-            if sigma > 100:
+            if self.sigma > 100:
                 wvel = disp2vel(w, velscale)
             ssp = data[:, 1:].T
-            if sigma <= 100:
-                newssp = spectres(wave, w, ssp)
+            if self.sigma <= 100:
+                newssp = spectres(self.wave, w, ssp)
             else:
                 ssp_rebin = spectres(wvel, w, ssp)
                 ssp_broad = gaussian_filter1d(ssp_rebin, kernel_sigma,
                                               mode="constant", cval=0.0)
-                newssp = spectres(wave, wvel, ssp_broad)
+                newssp = spectres(self.wave, wvel, ssp_broad)
 
             ssps.append(newssp)
         ssps = np.vstack(ssps)
@@ -209,32 +240,10 @@ class CvD18():
         return params, templates
 
 
-    def prepare_CvD18_respfun(self, rf_files, wave, outprefix, overwrite=False,
-                              sigma=100):
-        """ Prepare response functions from CvD models.
-
-        Parameters
-        ----------
-        rf_files: str
-            List of paths for the response function files.
-        wave: np.array
-            Wavelength dispersion for the output.
-        outprefix: str
-            First part of the name of the response function output files. The
-            response functions are stored in different files for different
-            elements, named "{}_{}.fits".format(outprefix, element).
-        overwrite: bool (optional)
-            Overwrite output.
-        """
-        if hasattr(wave, "unit"):
-            wave = wave.to(u.Angstrom).value
-        assert wave[0] >= 3501, "Minimum wavelength is 3501 Angstrom"
-        assert wave[-1] <= 25000, "Maximum wavelength is 25000 Angstrom"
-        assert sigma >= 100, "Minimum velocity dispersion is 100 km/s"
-        assert all([os.path.exists(f) for f in rf_files]), \
-               "Some or all input files are missing."
+    def _prepare_CvD18_respfun(self):
+        """ Prepare response functions from CvD models. """
         # Read one spectrum to get name of columns
-        with open(rf_files[0]) as f:
+        with open(self.rf_infiles[0]) as f:
             header = f.readline().replace("#", "")
         fields = [_.strip() for _ in header.split(",")]
         fields[fields.index("C+")] = "C+0.15"
@@ -246,12 +255,12 @@ class CvD18():
         elements = set([_.split("+")[0].split("-")[0] for _ in fields if
                         any(c in _ for c in ["+", "-"])])
         signal = ["+", "-"]
-        velscale = int(sigma / 4)
-        kernel_sigma = np.sqrt(sigma**2 - 100**2) / velscale
+        velscale = int(self.sigma / 4)
+        kernel_sigma = np.sqrt(self.sigma**2 - 100**2) / velscale
         rfsout = dict([(element, []) for element in elements])
         parsout = dict([(element, []) for element in elements])
         desc = "Preparing response functions"
-        for i, fname in enumerate(tqdm(rf_files, desc=desc)):
+        for i, fname in enumerate(tqdm(self.rf_infiles, desc=desc)):
             spec = os.path.split(fname)[1]
             T = float(spec.split("_")[2][1:])
             Z = float(spec.split("_")[3].split(".abun")[0][1:].replace(
@@ -259,35 +268,47 @@ class CvD18():
             data = np.loadtxt(fname)
             w = data[:, 0]
             data = data.T
-            if sigma > 100:
+            if self.sigma > 100:
                 wvel = disp2vel(w, velscale)
                 rebin = spectres(wvel, w, data)
                 broad = gaussian_filter1d(rebin, kernel_sigma,
                                           mode="constant", cval=0.0)
-                data = spectres(wave, wvel, broad).T
+                data = spectres(self.wave, wvel, broad).T
 
             else:
-                data = spectres(wave, w, data).T
+                data = spectres(self.wave, w, data).T
             fsun = data[:, 1]
             for element in elements:
                 # Adding solar response
                 p = Table([[Z], [T], [0.]], names=["Z", "Age", element])
-                rfsout[element].append(np.ones(len(wave)))
+                rfsout[element].append(np.ones(len(self.wave)))
                 parsout[element].append(p)
                 # Adding non-solar responses
                 for sign in signal:
                     name = "{}{}".format(element, sign)
-                    cols = [(i,f) for i, f in enumerate(fields) if f.startswith(
-                        name)]
+                    cols = [(i,f) for i, f in enumerate(fields) if
+                            f.startswith(name)]
                     for i, col in cols:
-                        val = float("{}1".format(sign)) * float(col.split(sign)[1])
-                        t = Table([[Z], [T], [val]], names=["Z", "Age", element])
+                        val = float("{}1".format(sign)) * \
+                              float(col.split(sign)[1])
+                        t = Table([[Z], [T], [val]],
+                                  names=["Z", "Age", element])
                         parsout[element].append(t)
                         rf = data[:, i] / fsun
                         rfsout[element].append(rf)
         rfs = dict([(e, np.array(rfsout[e])) for e in elements])
         rfpars = dict([(e, vstack(parsout[e])) for e in elements])
         return rfs, rfpars
+
+    @property
+    def limits(self):
+        """ Lower and upper limits of the model parameters. """
+        return self._limits
+
+    @property
+    def parnames(self):
+        """ Name of the parameters of the model. """
+        return self._parnames
 
 if __name__ == "__main__":
     pass
