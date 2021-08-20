0d2538873a6dd459a101123a2c7ea3439ceaa4e7
cebarbosa <kadu.barbosa@gmail.com>
2021-08-20 14:43:55 -0300

    Updating file to allow documentation.

--- paintbox/__init__.py
+++ paintbox/__init__.py
@@ -8,9 +8,9 @@ __all__ = ['__version__', 'test']
 from astropy.tests.runner import TestRunner
 test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
 
-from paintbox.operators import LOSVDConv, Resample
 from paintbox.sed import ParametricModel, NonParametricModel, Polynomial, \
                          CompoundSED
+from paintbox.operators import LOSVDConv, Resample
 from paintbox.extlaws import CCM89, C2000
 from paintbox.likelihoods import StudTLogLike, StudT2LogLike, NormalLogLike, \
                                  Normal2LogLike
@@ -18,3 +18,12 @@ from paintbox.ssp_utils import CvD18, MILES
 from paintbox.logspace_dispersion import disp2vel, logspace_dispersion
 from paintbox.convolve import broad2lick, broad2res
 from paintbox.version import version as __version__
+
+__all__ = ["ParametricModel", "NonParametricModel", "Polynomial",
+           "CompoundSED"]
+__all__ += ["LOSVDConv", "Resample"]
+__all__ += ["CCM89", "C2000"]
+__all__ += ["StudTLogLike", "StudT2LogLike", "NormalLogLike", "Normal2LogLike"]
+__all__ += ["CvD18", "MILES"]
+__all__ += ["disp2vel", "logspace_dispersion"]
+__all__ += ["broad2lick", "broad2res"]
